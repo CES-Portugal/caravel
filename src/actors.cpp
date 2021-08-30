fd20020dfec3e64f887a3eea70abbaee05fb5061
JoaoAMarinho <joao.andre.marinho@gmail.com>
2021-08-30 15:30:01 +0100

    fix: fixes send of messages bug as well as wrong times between messages

--- src/actors.cpp
+++ src/actors.cpp
@@ -4,22 +4,6 @@ using namespace std;
 using namespace caf;
 
 /* 
-behavior mirror(event_based_actor* self) {
-    // return the (initial) actor behavior
-    return {
-        // a handler for messages containing a single string
-        // that replies with a string
-        [=](const string& what) -> string {
-            cout << "Actor " << self->id() << " is waitting for input:\n";
-            cin.get();
-            // prints "Hello World!" via aout (thread-safe cout wrapper)
-            aout(self) << "I am actor " << self->id() << ", sending " << what << std::endl;
-            // reply "!dlroW olleH"
-            return string{what.rbegin(), what.rend()};
-        },
-    };
-}
-
 void blocking_calculator_fun(blocking_actor* self) {
   bool running = true;
   self->receive_while(running)( //
@@ -31,30 +15,6 @@ void blocking_calculator_fun(blocking_actor* self) {
         running = false;
       }
     });
-}
-
-void hello_world(event_based_actor* self, const actor& buddy, const actor& calculator) {
-    //Wait for input
-
-    // send "Hello World!" to our buddy ...
-    self->request(buddy, chrono::seconds(10), "Hello World!")
-        .then(
-        // ... wait up to 10s for a response ...
-        [=](const string& what) {
-            // ... and print it
-            aout(self) << "Received response: " << what << endl;
-        });
-    cout << "Going for calculator" << endl;
-    self->request(calculator, chrono::seconds(10), add_atom_v, 1, 2)
-        .then(
-          [&](int32_t what) {
-            // ... and print it
-            cout << "Received response: " << what << endl;  
-        },
-        [&](error& err) {
-          aout(self) << "cell #" << to_string(err) << endl;
-        }
-        );
 }
 
 behavior send_cyclic_messages(event_based_actor* self,int num, int delay) {
@@ -70,14 +30,7 @@ behavior send_cyclic_messages(event_based_actor* self,int num, int delay) {
         after(chrono::seconds(3)) >> [] { cout << "Cyclic messaging!\n"; }
     };
 }
-
-void change_v(event_based_actor* self,const actor& buddy) {
-    //Wait for input
-    int n = cin.get()-'0';
-    // send "Hello World!" to our buddy ...
-    self->send_exit(buddy, exit_reason::kill);
-    self->spawn(send_cyclic_messages, 2, n);
-} */
+ */
 
 
 CAF_BEGIN_TYPE_ID_BLOCK(custom_types_1, first_custom_type_id)
@@ -152,98 +105,78 @@ int setup_socket(int& skt){
 }
 
 //Receiving messages
-void receive_msg(event_based_actor* self, const int& skt, const group& g1){
+void receive_msg(event_based_actor* self, const int& skt, const group& grp){
     int nbytes;
     struct can_frame frame;
 
-    aout(self) << "Actor nº: "<< self->id() <<". Is reading data from socket!\n";
-    //while (1)
+    aout(self) << "Actor nº: "<< self->id() <<". Is reading data from socket!" << endl;
+    while (1)
     {
         nbytes = read(skt, &frame, sizeof(frame));
 
         if (nbytes > 0)
         {
-            self->send(g1,frame);
-            //self->send(g1,12);  //Test purposes
+            self->send(grp,frame);
             nbytes=0;
         }
     }
     close(skt);
-    self->send_exit(g1, exit_reason::normal);
+    self->send_exit(grp, exit_reason::normal);
+}
+
+behavior log_message(event_based_actor* self, const bool& rcv_msg, const chrono::steady_clock::time_point& start){
+
+    //pcpp::PcapFileWriterDevice pcapWriter("./logs/output.pcap", pcpp::LINKTYPE_CAN_SOCKETCAN);
+
+    auto setup_vars = [&, self] {
+        /* if (!pcapWriter.open())
+        {
+            aout(self) << "Cannot open output.pcap for writing" << endl;
+            exit(1);
+        } */
+    };
+
+    return {
+        [=](const can_frame& frame) {
+            struct timespec time;
+            chrono::steady_clock::time_point end = chrono::steady_clock::now();
+
+            //setup_vars();
+
+            if(!rcv_msg)
+                end=start;
+
+            elapsed_time(start, end, time);
+                
+            /* pcpp::RawPacket rawPacket(frame.data, frame.can_dlc, time,false, pcpp::LINKTYPE_CAN_SOCKETCAN);
+            pcapWriter.writePacket(rawPacket); */
+            aout(self) << time.tv_sec << endl;
+            if(!rcv_msg) self->become(log_message(self, true, chrono::steady_clock::now()));
+            //MISING CLOSE PCAPWRITER
+        },
+    };
 }
 
 behavior output_message(event_based_actor* self) {
     return {
-        [&](const can_frame& frame) {
-            //Output message
-            printf("can_id  = 0x%X\r\ncan_dlc = %d \r\n", frame.can_id, frame.can_dlc);
-            //aout(self) << "can_id = 0x" << setbase(16) <<  frame.can_id << "\r\ncan_dlc = "<< frame.can_dlc << " \r\n";
-            int i = 0;
-            for (i = 0; i < 8; i++)
-                aout(self) << "data[" << i << "] = "<< frame.data[i] <<"\r\n";
+        [&, self](const can_frame& frame) {
+            stringstream sstream;
+            sstream << hex << frame.can_id;
             
-        },
-        [](const int& frame) {
-            //Output message
-            printf("number = %d \r\n", frame);
-        },
-    };
-}
-
-behavior log_message(event_based_actor* self,int n_msg){
-    
-    struct timespec time;
-    chrono::steady_clock::time_point start=chrono::steady_clock::now();
-
-    pcpp::PcapFileWriterDevice pcapWriter("./logs/output.pcap", pcpp::LINKTYPE_CAN_SOCKETCAN);
-
-    auto setup_vars = [&] {
-        if (!pcapWriter.open())
-        {
-            printf("Cannot open output.pcap for writing\n");
-            exit(1);
-        }
-    };
-    return {
-        [&](const can_frame& frame) {
-            if (n_msg == 0){
-                setup_vars();
-                elapsed_time(start, start, time);
-                start = chrono::steady_clock::now();
-            }
-            else{
-                chrono::steady_clock::time_point end = chrono::steady_clock::now();
-                elapsed_time(start, end, time);
-            }
-            pcpp::RawPacket rawPacket(frame.data, frame.can_dlc, time,false, pcpp::LINKTYPE_CAN_SOCKETCAN);
-            pcapWriter.writePacket(rawPacket);
-            log_message(self, n_msg+1);
-            //MISING CLOSE PCAPWRITER
-        },
-        [&](const int& frame) {
-            printf("number = %d \r\n", frame);
-            if (n_msg == 0){
-                setup_vars();
-                elapsed_time(start, start, time);
-                start = chrono::steady_clock::now();
-            }
-            else{
-                chrono::steady_clock::time_point end = chrono::steady_clock::now();
-                elapsed_time(start, end, time);
-            }
-            const uint8_t* aptr;
-            pcpp::RawPacket rawPacket(aptr, 2, time,false, pcpp::LINKTYPE_CAN_SOCKETCAN);
-            pcapWriter.writePacket(rawPacket);
-            log_message(self, n_msg+1);
-            //MISING CLOSE PCAPWRITER
+            //Output message details
+            aout(self) << "\nMessage details:" << endl;
+            aout(self) << "can_id  = 0x" << sstream.str() <<" \r" << endl;
+            aout(self) << "can_dlc = " << frame.can_dlc << " \r" << endl;
+            aout(self) << "can_data = " << frame.data << " \r" << endl;
         },
     };
 }
+
 
 //Sending messages
-void delegate_send_msg(event_based_actor* self, const actor& send_act){
+void delegate_send_msg(event_based_actor* self, const group& grp){
 
-    aout(self) << "Actor nº: "<< self->id() <<". Is waitting for input!\n";
+    aout(self) << "Actor nº: "<< self->id() <<". Is waitting for input!" << endl;
     
     struct can_frame frame;
     string line;
@@ -263,29 +196,24 @@ void delegate_send_msg(event_based_actor* self, const actor& send_act){
         msgAscii = hex_to_ascii(msgAscii);
         frame.can_dlc = msgAscii.size();
         strcpy((char*)frame.data,msgAscii.c_str());
-
-        //Output message details
-        printf("\nMessage details:\n");
-        printf("can_id  = 0x%X\r\n", frame.can_id);
-        printf("can_dlc = %d\r\n", frame.can_dlc);
-        printf("can_data = %s\r\n", frame.data);
-
-        self->send(send_act, frame);
+        
+        self->send(grp, frame);
     }
 
 }
 
 behavior send_message(event_based_actor* self, const int& skt){
     return {
-        [&](const can_frame& frame) {
+        [=](const can_frame& frame) {
             int nbytes;
+
             nbytes = write(skt, &frame, sizeof(struct can_frame)); 
             if(nbytes != sizeof(struct can_frame)) {
-                printf("Send Error!\r\n");
+                aout(self) << "Send Error!\r" << endl;
                 self->quit();
             }
-            //aout(self) << "Message sent with success!\n";
-            cout  << "Message sent with success!\n\n";;
+            
+            aout(self) << "Message sent with success!\n" << endl;
         },
     };
 }
@@ -296,28 +224,18 @@ void caf_main(actor_system& sys) {
 
     if (setup_socket(skt)) return;
 
-    auto grp = sys.groups().get_local("receiver");
+    auto rcv_grp = sys.groups().get_local("receiver");
+    auto send_grp = sys.groups().get_local("sender");
 
-    auto output_act = sys.spawn_in_groups({grp}, output_message);
-    auto log_act = sys.spawn_in_groups({grp}, log_message, 0);
-    auto send_act = sys.spawn(send_message, skt);
+    auto output_act = sys.spawn_in_groups({rcv_grp, send_grp}, output_message);
+    auto log_act = sys.spawn_in_groups({rcv_grp}, log_message, false, chrono::steady_clock::now());
 
-    auto receiver = sys.spawn(receive_msg, skt, grp);
-    auto sender = sys.spawn(delegate_send_msg, send_act);
+    auto send_act = sys.spawn_in_groups({send_grp}, send_message, skt);
 
+    auto receiver = sys.spawn(receive_msg, skt, rcv_grp);
+    auto sender = sys.spawn(delegate_send_msg, send_grp);
 
-/*     auto cyclic_actor = sys.spawn(send_cyclic_messages, 1, 1);
-    sys.spawn(change_v, cyclic_actor);
-
-    // create a new actor that calls 'mirror()'
-    auto mirror_actor = sys.spawn(mirror);
-    
-    auto calc_actor = sys.spawn(blocking_calculator_fun);
-    
-    // create another actor that calls 'hello_world(mirror_actor)';
-    sys.spawn(hello_world, mirror_actor, calc_actor);
-    // the system will wait until both actors are done before exiting the program
-    scoped_actor self{sys};
+/*  scoped_actor self{sys};
     self->send_exit(calc_actor, exit_reason::kill); */
 }
 
