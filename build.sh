380329958ec1d04d7d2480ce949dcbf0d87ea079
JoaoMarinho <joao.andre.marinho@gmail.com>
2021-08-31 18:01:58 +0100

    feat: adds cross compile from windows to raspberry pi (not finished)

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ rm -rf build
 mkdir build
 pushd build
 
-conan install .. --build=missing
-cmake .. -DCMAKE_BUILD_TYPE=Debug
+conan install .. --build=missing --profile:host ../win_to_rpi --profile:build ../default_build_conf
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../toolchain-arm.cmake
 cmake --build .
 
 cp ./bin/caravel ../
